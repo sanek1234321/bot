667f6870d134075271ca50e1f4ba4a69be08e7a4
sanek1234321 <104680898+sanek1234321@users.noreply.github.com>
2022-04-30 20:40:01 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -5,17 +5,17 @@ parser = argparse.ArgumentParser()
 
 import requests
 
-parser.add_argument("token", help = "token234567")
+parser.add_argument("token", help = "token")
 args = parser.parse_args()
 
 bot = telebot.TeleBot(args.token)
 
-@bot.message_handler(commands=['getUSD'])
+@bot.message_handler(commands=['get'])
 def start_message(message):
-    bot.send_message(message.chat.id, data['Valute']['USD']['Value'])
-@bot.message_handler(commands=['getEUR'])
-def start_message(message):
-    bot.send_message(message.chat.id, data['Valute']['EUR']['Value'])
+    bot.send_message(message.chat.id, 'напиши название валюты (USD,EUR...)')
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    bot.send_message(message.chat.id, data['Valute'][message.text]['Value'])
 
 bot.polling()
 
