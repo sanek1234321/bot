82f1de4d7216e5e83df3f63656bfce5f527099fb
sanek1234321 <104680898+sanek1234321@users.noreply.github.com>
2022-04-30 19:09:51 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -11,8 +11,12 @@ args = parser.parse_args()
 bot = telebot.TeleBot(args.token)
 
 data = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
-@bot.message_handler(commands=['get'])
+@bot.message_handler(commands=['getUSD'])
 def start_message(message):
     bot.send_message(message.chat.id, data['Valute']['USD']['Value'])
+@bot.message_handler(commands=['getEUR'])
+def start_message(message):
+    bot.send_message(message.chat.id, data['Valute']['EUR']['Value'])
 
 bot.polling()
+
